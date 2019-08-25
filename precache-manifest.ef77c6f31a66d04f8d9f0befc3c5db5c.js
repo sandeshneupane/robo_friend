70f444515c36d432b0caf0c152f0e3eb91c7e2a7
sandeshneupane <sandesh.neupane4@gmail.com>
2019-08-25 14:37:03 -0500

    Updates

---
+++ precache-manifest.ef77c6f31a66d04f8d9f0befc3c5db5c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bb431553981178e9e352e3061a2bdda",
+    "url": "/robo_friend/index.html"
+  },
+  {
+    "revision": "c907e6c068f828c266b4",
+    "url": "/robo_friend/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "853c3c00a78f14bfc1da",
+    "url": "/robo_friend/static/css/main.a278dbc5.chunk.css"
+  },
+  {
+    "revision": "c907e6c068f828c266b4",
+    "url": "/robo_friend/static/js/2.ef70dbb2.chunk.js"
+  },
+  {
+    "revision": "853c3c00a78f14bfc1da",
+    "url": "/robo_friend/static/js/main.1da8cb26.chunk.js"
+  },
+  {
+    "revision": "dc1553c1b691c37314d7",
+    "url": "/robo_friend/static/js/runtime~main.ecc8cd1a.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo_friend/static/media/SEGA.12a3db8b.woff"
+  }
+]);
